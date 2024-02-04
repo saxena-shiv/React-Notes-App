0f1d4cc130c342088c434fd4bb4c8abdb1120943
Shivam Saxena <secureshiv22@gmail.com>
2024-02-04 09:16:44 +0530

    my first commit
    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Foreground from "./component/Foreground"
+import Header from "./component/Header"
+
+// import React from 'react'
+
+function App() {
+	return (
+		<>
+			<div className="relative text-white bg-zinc-800 w-full h-screen">
+				<Header />
+				<Foreground/>
+			</div>
+		</>
+	)
+}
+
+export default App 
