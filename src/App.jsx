1be9152782413940c14206ddaf31130c91333992
Shivam Saxena <shivam2003official@gmail.com>
2024-02-11 14:23:16 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,28 @@ import Header from "./component/Header"
 // import React from 'react'
 
 function App() {
+<<<<<<< HEAD
+	
+    const farFutureDate = new Date('2038-01-01T00:00:00Z');
+=======
+	const farFutureDate = new Date('2038-01-01T00:00:00Z');
+>>>>>>> eecf2bb66a769665376673b2b2d2f9953f7832e7
+	useEffect(() => {
+		const cookie = () => {
+			window.onload = () => {
+				if (document.cookie === '')
+				{
+					document.cookie = `i = 0; expires = ${farFutureDate.toUTCString()}; priority = high`;
+				}
+			}
+			
+		}
+		cookie();
+	}, [])
+<<<<<<< HEAD
+	
+=======
+>>>>>>> eecf2bb66a769665376673b2b2d2f9953f7832e7
 	return (
 		<>
 			<div className="relative text-white bg-zinc-800 w-full h-screen">
